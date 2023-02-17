0f0ff95dbdbee7c2de8d98be40e5d8055e3cc297
MindulaHewadewage <mindufernando@gmail.com>
2023-02-17 17:40:18 +0100

    var dump dei film

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-class Features{
+class Features
+{
     // proprietà o variabili d'istanza
     public $title;
     public $movie_director;
@@ -9,25 +10,24 @@ class Features{
     public $country_of_production;
     public $production_year;
 
-    public function __construct($_title,$_movie_director,$_original_language,$_genre,$_plot,$_country_of_production,$_production_year)
+    public function __construct($_title, $_movie_director, $_original_language, $_genre, $_plot, $_country_of_production, $_production_year)
     {
-        echo 'costruttore';
+        $this->title = $_title;
+        $this->movie_director = $_movie_director;
+        $this->original_language = $_original_language;
+        $this->genre = $_genre;
+        $this->plot = $_plot;
+        $this->country_of_production = $_country_of_production;
+        $this->production_year = $_production_year;
     }
-
 }
 
-$Interstellar=new Features();
+$interstellar = new Features('Interstellar', 'Christopher Nolan', 'English', 'Fantasy', '...', 'United States', 2014);
 
-$Inception=new Features();
+$inception = new Features('Inception', '	Christopher Nolan', 'English', 'Thriller', '...', 'United States', 2010);
 
-$Avatar=new Features();
+$avatar = new Features('Avatar', 'James Cameron', 'English', 'Fantasy', '...', 'United States', 2009);
 
-
-
-
-
-
-
-
-
-?>
+var_dump($interstellar);
+var_dump($inception);
+var_dump($avatar);
